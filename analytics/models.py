afb7ffdd6757a48b26615aad59d4d01929cf6515
Issac Kelly <issac.kelly@gmail.com>
2011-06-24 21:27:31 -0400

    Add BSD License, and Apologize about some weirdness with Model required to make a ModelAdmin... I'm much more familair with the Django Admin now, and there is a better way to do this.

--- analytics/models.py
+++ analytics/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 
 class Track(models.Model):
+    # There is a better way to do this, just not implemented yet
     name = models.CharField(max_length=1, help_text="THIS SHOULD NOT BE USED")
